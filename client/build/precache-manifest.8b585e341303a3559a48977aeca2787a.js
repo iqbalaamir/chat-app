a800942c9cc14a28eaa5200d9d9419e30e063756
iqbalaamir <aamir.iqbal040@gmail.com>
2023-07-03 08:05:15 +0530

    build

---
+++ client/build/precache-manifest.8b585e341303a3559a48977aeca2787a.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "df897baaed69bb23f3d135866ff1510c",
+    "url": "/index.html"
+  },
+  {
+    "revision": "d87c9284e91bc9f9c160",
+    "url": "/static/css/main.d194dd61.chunk.css"
+  },
+  {
+    "revision": "854186ce1c1d4356a220",
+    "url": "/static/js/2.ef321e1b.chunk.js"
+  },
+  {
+    "revision": "bf744d37492ffed4d3003a8f244c622c",
+    "url": "/static/js/2.ef321e1b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d87c9284e91bc9f9c160",
+    "url": "/static/js/main.13bf752b.chunk.js"
+  },
+  {
+    "revision": "9b4e396c183e42c1fa5c",
+    "url": "/static/js/runtime-main.09b85ec0.js"
+  }
+]);
--- server.js
+++ server.js
@@ -57,6 +57,7 @@ if(process.env.NODE_ENV=='production')
 {
   const path = require('path')
   app.get('/',(req,res)=>{
+    app.use(express.static(path.resolve(__dirname,'client','build')))
     res.sendFile(path.resolve(__dirname,'client','build','index.html'))
   })
 }
